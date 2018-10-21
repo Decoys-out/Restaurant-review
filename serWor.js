d9b8921a32015e8cbd0c78864b8852787199401d
Gaganjot Singh <32265569+Decoys-out@users.noreply.github.com>
2018-10-21 23:45:14 +0530

    Uploaded files

--- serWor.js
+++ serWor.js
@@ -1,30 +1,29 @@
-const cacheFile=['/','/index.html','/css/styles.css','/js/dbhelper.js','/js/main.js','/js/restaurant_info.js','/serWor.js','/data/restaurants.json','/img/1.jpg','/img/2.jpg','/img/3.jpg','/img/4.jpg','/img/5.jpg','/img/6.jpg','/img/7.jpg','/img/8.jpg','/img/9.jpg','/img/10.jpg','/restaurant.html'];
-self.addEventListener('install',function(e){
+const cacheFile = ['/', '/index.html', '/css/styles.css', '/js/dbhelper.js', '/js/main.js', '/js/restaurant_info.js', '/serWor.js', '/data/restaurants.json', '/img/1.jpg', '/img/2.jpg', '/img/3.jpg', '/img/4.jpg', '/img/5.jpg', '/img/6.jpg', '/img/7.jpg', '/img/8.jpg', '/img/9.jpg', '/img/10.jpg', '/restaurant.html'];
+self.addEventListener('install', function (e) {
     e.waitUntil(
-        caches.open('v1').then(function(cache){
+        caches.open('v1').then(function (cache) {
             return cache.addAll(cacheFile);
         })
     );
 });
-self.addEventListener('fetch',function(e){
+self.addEventListener('fetch', function (e) {
     e.respondWith(
-        caches.match(e.request).then(function(response){
-            if(response){
-                console.log('Found',e.request,' in cache');
+        caches.match(e.request).then(function (response) {
+            if (response) {
+                console.log('Found', e.request, ' in cache');
                 return response;
-            }
-            else{
-                console.log('Could not find',e.request,' in cache.');
-                return fetch(e.request).then(function(response){
-                     const response_clone=response.clone();  
-                    caches.open('v1').then(function(cache){
-                        cache.put(e.request,response_clone);
+            } else {
+                console.log('Could not find', e.request, ' in cache.');
+                return fetch(e.request).then(function (response) {
+                    const response_clone = response.clone();
+                    caches.open('v1').then(function (cache) {
+                        cache.put(e.request, response_clone);
                     })
                     return response;
-                }).catch(function(err){
+                }).catch(function (err) {
                     console.error(err);
                 });
             }
-        }) 
+        })
     );
 });
